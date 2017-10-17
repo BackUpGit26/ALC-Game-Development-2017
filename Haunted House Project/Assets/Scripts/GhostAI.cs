4d1b7a279200301cd2ae0bd2709729b3728a0f27
Jaedon Lippert <jaedonklb@gmail.com>
2017-10-17 13:02:38 -0600

    Flashlight

---
+++ Haunted House Project/Assets/Scripts/GhostAI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GhostAI : MonoBehaviour {
+
+	public float moveSpeed;
+	
+	public Transform target;
+	
+	public int damage;
+
+	// A trigger is an entity waiting for something to trigger it and it will do something when it is triggered
+	void OnTriggerStay(Collider other)
+	{
+		if(other.gameObject.name == "Player"){
+			transform.LookAt(target);
+			transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
+		}
+	}
+}
