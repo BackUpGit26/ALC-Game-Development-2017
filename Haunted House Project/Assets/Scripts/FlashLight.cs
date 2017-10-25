3865a2e5e9c21761463110eef1ca4d00b4ace9b3
Jaedon Lippert <jaedonklb@gmail.com>
2017-10-25 13:11:42 -0600

    Added On/Off Flashlight

---
+++ Haunted House Project/Assets/Scripts/FlashLight.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FlashLight : MonoBehaviour {
+
+	public bool LightOn;
+
+	Light light;
+
+	// Use this for initialization
+	void Start () {
+		light = getComponent<Light> ();
+		// Set Light default to ON
+		lightOn = true;
+		light.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Toggle light on/off when L key is pressed
+		if (Input.GetKeyUp (KeyCode.L) && lightOn) {
+			lightOn = false;
+			light.enabled = false;
+
+		}
+
+		else if (Input.GetKeyUp (KeyCode.L) && !lightOn){
+			lightOn = true;
+			light.enabled = true;
+		}
+	}
+}
