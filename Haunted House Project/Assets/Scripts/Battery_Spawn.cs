30cbc19df90e6130487ef206b18b91b4d9e2e295
Jaedon Lippert <jaedonklb@gmail.com>
2017-12-15 13:09:19 -0700

    Battery lol

---
+++ Haunted House Project/Assets/Scripts/Battery_Spawn.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Battery_Spawn : MonoBehaviour {
+
+
+	
+	public GameObject battery;
+
+	public Transform spawnPoint;
+
+	public float spawnTime;
+
+	public bool batSpawned;
+
+
+	// Use this for initialization
+	void Start () {
+		batSpawned = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(batSpawned == false){
+			StartCoroutine(SpawnBat())
+		}
+	
+	}
+	void OnTriggerEnter(){
+		if(other.GameObject.tag == "battery"){
+			batSpawned = true;
+		}
+		else{
+			batSpawned = false;
+		}
+	}
+	IEnumerator SpawnBat(){
+		yield return new WaitForSeconds(spawnTime);
+		Rigidbody clone;
+		(Rigidbody)Instantiate(battery, spawnPoint.position, spawnPoint.Rotation);
+	}
+
+	}
+}
